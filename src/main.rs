116b60073667aa97be8c1ef7c1993faf2eb00a86
1839b <jack@bob.sh>
2023-03-19 15:41:03 +0000

    trying stuff

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 use std::{
 	process,
-	io::{stdout, Write},
+	io::{stdin, stdout, Write},
 };
-
-
 
 mod user;
 
 fn main() {
-	let mut stdout = stdout();
-
 	let mut user = user::User::default();
+	let mut stdout = stdout();
 	loop {
 		let mut input = String::new();
-		std::io::stdin().read_line(&mut input).unwrap();
-
+		stdin().read_line(&mut input).unwrap();		
+	
 		match input.trim() {
 			"" => {			
 				user.click();
-				print!("\r");
-   				print!("{}", user.score());
+   				print!("\r{}", user.score());
 				stdout.flush().unwrap();
 			},	
 			_ => process::exit(0),
