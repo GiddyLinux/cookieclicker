1000b3e56628148065398cdd39dba73c4ac32e28
1839b <jack@bob.sh>
2023-03-19 15:02:43 +0000

    whoops fixed indentation

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
 				write!(stdout, "cookies").unwrap();			
 				user.click();
 				print!("\r");
-    print!("{}", user.score());
+   				print!("{}", user.score());
 				stdout.flush().unwrap();
 			},	
 			_ => process::exit(0),
