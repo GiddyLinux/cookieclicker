92666e39793529eb96fd79996e686534373db6b0
1839b <jack@bob.sh>
2023-03-19 16:34:52 +0000

    i figured out something dumb

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ mod user;
 
 fn main() {
 	let mut user = user::User::default();
-	let mut stdout = stdout();
-	loop {
-		let mut input = String::new();
-		stdin().read_line(&mut input).unwrap();		
-	
+	let input = &mut String::new();
+
+	loop {	
+		input.clear();
+		stdin().read_line(input).unwrap();
+
 		match input.trim() {
 			"" => {			
 				user.click();
-   				print!("\r{}", user.score());
-				stdout.flush().unwrap();
+   				print!("{}", user.score());
+				stdout().flush().unwrap();
 			},	
 			_ => process::exit(0),
 		}
